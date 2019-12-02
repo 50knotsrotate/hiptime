ce4ea73eb9b68054f325fbde19b5c56ae8d33699
= <=>
2019-12-02 12:42:12 -0500

    show, edit, update todos

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,42 @@
+class ItemsController < ApplicationController
+    def index 
+        @items = Item.all.order('created_at DESC')
+    end
+
+    def new
+        @item = Item.new
+    end
+
+    def create
+         @item = Item.new(item_params)
+         if @item.save
+            redirect_to root_path
+
+         else 
+            render 'new'
+         end
+    end
+
+    def show
+        @item = Item.find(params[:id])
+    end
+
+    def edit
+        @item = Item.find(params[:id])
+    end
+
+    def update
+        @item = Item.find(params[:id])
+        if @item.save
+            redirect_to root_path
+        else
+            render 'edit'
+        end
+    end
+
+    private 
+
+    def item_params
+        return params.require(:item).permit(:title, :description)
+    end
+end
